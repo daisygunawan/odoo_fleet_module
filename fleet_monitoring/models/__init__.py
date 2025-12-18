6db65e9e251a3d8d114e716f657cceb2c636ebf0
Daisy Gunawan <147494361+daisygunawan@users.noreply.github.com>
2025-12-18 09:24:09 +0700

    Delete fleet_monitoring directory

--- fleet_monitoring/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import vehicle_checklist_car
-from . import vehicle_checklist_motor
-from . import vehicle_usage
-from . import vehicle_service
-from . import vehicle_document
-from . import stock_picking  
